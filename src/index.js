0fca9060393327a10bba9d0c2ac9e322ba0f2c23
Alexander Rogozhkin <alexanderrogozhkin@Alexanders-MacBook-Air.local>
2020-09-29 22:30:36 +0500

    remove some libs. only pixi.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,22 @@
-// Import Application class that is the main part of our PIXI project
-import { Application } from "@pixi/app";
+import * as PIXI from "pixi.js";
 
-// In order that PIXI could render things we need to register appropriate plugins
-import { Renderer } from "@pixi/core"; // Renderer is the class that is going to register plugins
+const app = new PIXI.Application();
 
-import { BatchRenderer } from "@pixi/core"; // BatchRenderer is the "plugin" for drawing sprites
-Renderer.registerPlugin("batch", BatchRenderer);
+document.body.appendChild(app.view);
 
-import { TickerPlugin } from "@pixi/ticker"; // TickerPlugin is the plugin for running an update loop (it's for the application class)
-Application.registerPlugin(TickerPlugin);
+app.loader.add("logo", "assets/logo.png").load((loader, resources) => {
+  const logo = new PIXI.Sprite(resources.logo.texture);
 
-// And just for convenience let's register Loader plugin in order to use it right from Application instance like app.loader.add(..) etc.
-import { AppLoaderPlugin } from "@pixi/loaders";
-Application.registerPlugin(AppLoaderPlugin);
+  logo.x = app.renderer.width / 2;
+  logo.y = app.renderer.height / 2;
 
-// Sprite is our image on the stage
-import { Sprite } from "@pixi/sprite";
+  logo.anchor.x = 0.5;
+  logo.anchor.y = 0.5;
 
-// App with width and height of the page
-const app = new Application({
-  width: window.innerWidth,
-  height: window.innerHeight,
-});
-document.body.appendChild(app.view); // Create Canvas tag in the body
+  // app.stage.appChild(logo);
+  app.stage.addChild(logo);
 
-// Load the logo
-app.loader.add("logo", "./assets/logo.png");
-app.loader.load(() => {
-  const sprite = Sprite.from("logo");
-  sprite.anchor.set(0.5); // We want to rotate our sprite relative to the center, so 0.5
-  app.stage.addChild(sprite);
-
-  // Position the sprite at the center of the stage
-  sprite.x = app.screen.width * 0.5;
-  sprite.y = app.screen.height * 0.5;
-
-  // Put the rotating function into the update loop
-  app.ticker.add((delta) => {
-    sprite.rotation += 0.02 * delta;
+  app.ticker.add(() => {
+    logo.rotation += 0.01;
   });
 });
