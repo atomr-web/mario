be3393ec9bd4db939ed3e8e6df59870c6f1a767c
Alexander Rogozhkin <alexanderrogozhkin@Alexanders-MacBook-Air.local>
2020-09-29 22:44:24 +0500

    init app sizes, color

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import * as PIXI from "pixi.js";
 
-const app = new PIXI.Application();
+let app = new PIXI.Application({
+  width: 512,
+  height: 512,
+});
+
+app.renderer.backgroundColor = 0x061639;
 
 document.body.appendChild(app.view);
-
-app.loader.add("logo", "assets/logo.png").load((loader, resources) => {
-  const logo = new PIXI.Sprite(resources.logo.texture);
-
-  logo.x = app.renderer.width / 2;
-  logo.y = app.renderer.height / 2;
-
-  logo.anchor.x = 0.5;
-  logo.anchor.y = 0.5;
-
-  // app.stage.appChild(logo);
-  app.stage.addChild(logo);
-
-  app.ticker.add(() => {
-    logo.rotation += 0.01;
-  });
-});
